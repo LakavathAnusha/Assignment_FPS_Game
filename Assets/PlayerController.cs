043ae5329b666afb9e143e25cd9fcfb175f96e1c
PTRL483\Anusha.Lakavath <anusha.lakavath@yesgnome.com>
2022-04-18 23:50:13 +0530

    Cannimal is Created

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float playerSpeed;
+    public float playerJumpForce;
+    public float playerRotationSpeed;
+    //public Transform bulletLaunch;
+    //public GameObject steveModelPrefabs;
+    Rigidbody rb;
+    public Animator animator;
+    CapsuleCollider colliders;
+    Quaternion camRotation;
+    public Camera cam;
+    Quaternion playerRotation;
+    float minX = -90f;
+    float maxX = 90f;
+
+    //SpawnManager spawnManager;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        colliders = GetComponent<CapsuleCollider>();
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetBool("IsShoot", !animator.GetBool("IsShoot"));
+        }
+    }
+    private void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal") * playerSpeed;
+        float inputz = Input.GetAxis("Vertical") * playerSpeed;
+
+        transform.position += new Vector3(inputX, 0f, inputz);
+
+        float mouseX = Input.GetAxis("Mouse X") * playerRotationSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * playerRotationSpeed;
+        //Debug.Log(mouseY);
+        playerRotation = Quaternion.Euler(0f, mouseX, 0f);
+        camRotation = Quaternion.Euler(-mouseY, 0f, 0f);
+        camRotation = ClampRotationPlayer(camRotation);
+        // this.transform.localRotation = playerRotation;
+        transform.localRotation = playerRotation * transform.localRotation;
+        cam.transform.localRotation = camRotation * cam.transform.localRotation;
+
+    }
+
+    public Quaternion ClampRotationPlayer(Quaternion n)
+    {
+
+        n.w = 1f;
+        n.x /= n.w;
+        n.y /= n.w;
+        n.z /= n.w;
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(n.x);
+
+        angleX = Mathf.Clamp(angleX, minX, maxX);
+        n.x = Mathf.Tan(Mathf.Deg2Rad * angleX * 0.5f);
+        return n;
+    }
+}
